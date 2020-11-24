71c7eb370602a96990eb22af4b3d8a886c5d5af8
Volker Bohne <bohne@centrias-colocation.de>
2020-11-24 17:33:14 +0100

    fmt now

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,9 +1,9 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket_policy" "this" {
-  bucket = var.bucket_name
-  depends_on      = [var.bucket_name]
-  policy = <<POLICY
+  bucket     = var.bucket_name
+  depends_on = [var.bucket_name]
+  policy     = <<POLICY
 {
     "Version": "2012-10-17",
     "Statement": [
