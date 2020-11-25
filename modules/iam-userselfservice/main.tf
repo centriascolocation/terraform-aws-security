ff755b38628b4d11dde289d9d8883e4f21c366be
Michael Krieg <michael@centrias.de>
2020-11-25 17:05:22 +0100

    describe outputs

---
+++ modules/iam-userselfservice/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_policy" "this" {
+  name        = var.policy_name
+  description = var.policy_description
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowViewAccountInfo",
+            "Effect": "Allow",
+            "Action": [
+                "iam:GetAccountPasswordPolicy",
+                "iam:GetAccountSummary"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowManageOwnPasswords",
+            "Effect": "Allow",
+            "Action": [
+                "iam:ChangePassword",
+                "iam:GetUser"
+            ],
+            "Resource": "arn:aws:iam::*:user/$${aws:username}"
+        },
+        {
+            "Sid": "AllowManageOwnAccessKeys",
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateAccessKey",
+                "iam:DeleteAccessKey",
+                "iam:ListAccessKeys",
+                "iam:UpdateAccessKey"
+            ],
+            "Resource": "arn:aws:iam::*:user/$${aws:username}"
+        },
+        {
+            "Sid": "AllowManageOwnSSHPublicKeys",
+            "Effect": "Allow",
+            "Action": [
+                "iam:DeleteSSHPublicKey",
+                "iam:GetSSHPublicKey",
+                "iam:ListSSHPublicKeys",
+                "iam:UpdateSSHPublicKey",
+                "iam:UploadSSHPublicKey"
+            ],
+            "Resource": "arn:aws:iam::*:user/$${aws:username}"
+        }
+    ]
+}
+  EOF
+}
---
+++ modules/iam-userselfservice/variables.tf
@@ -0,0 +1,9 @@
+variable "policy_name" {
+  description = "Name of the IAM Policy"
+  type        = string
+}
+
+variable "policy_description" {
+  description = "Description of the IAM Policy"
+  type        = string
+}
