fed9ebb8ade82c07c8684ac36bf1f55594831f84
Volker Bohne <bohne@centrias-colocation.de>
2020-11-30 13:50:43 +0100

    changed policy from heredoc to tf data block and jsonencode added MFA policy actions to handle MFA devices as self service

--- modules/iam-userselfservice/main.tf
+++ modules/iam-userselfservice/main.tf
@@ -2,52 +2,83 @@ resource "aws_iam_policy" "this" {
   name        = var.policy_name
   description = var.policy_description
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowViewAccountInfo",
-            "Effect": "Allow",
-            "Action": [
-                "iam:GetAccountPasswordPolicy",
-                "iam:GetAccountSummary"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "AllowManageOwnPasswords",
-            "Effect": "Allow",
-            "Action": [
-                "iam:ChangePassword",
-                "iam:GetUser"
-            ],
-            "Resource": "arn:aws:iam::*:user/$${aws:username}"
-        },
-        {
-            "Sid": "AllowManageOwnAccessKeys",
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateAccessKey",
-                "iam:DeleteAccessKey",
-                "iam:ListAccessKeys",
-                "iam:UpdateAccessKey"
-            ],
-            "Resource": "arn:aws:iam::*:user/$${aws:username}"
-        },
-        {
-            "Sid": "AllowManageOwnSSHPublicKeys",
-            "Effect": "Allow",
-            "Action": [
-                "iam:DeleteSSHPublicKey",
-                "iam:GetSSHPublicKey",
-                "iam:ListSSHPublicKeys",
-                "iam:UpdateSSHPublicKey",
-                "iam:UploadSSHPublicKey"
-            ],
-            "Resource": "arn:aws:iam::*:user/$${aws:username}"
-        }
-    ]
+  policy = data.aws_iam_policy_document.this.json
 }
-  EOF
+
+data "aws_iam_policy_document" "this" {
+
+  version = "2012-10-17"
+  statement {
+    sid    = "AllowViewAccountInfo"
+    effect = "Allow"
+    actions = [
+      "iam:GetAccountPasswordPolicy",
+      "iam:GetAccountSummary",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowManageOwnPasswords"
+    effect = "Allow"
+    actions = [
+      "iam:ChangePassword",
+      "iam:GetUser",
+    ]
+    resources = [
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "IAMAccessKeyPolicy"
+    effect = "Allow"
+    actions = [
+      "iam:CreateAccessKey",
+      "iam:DeleteAccessKey",
+      "iam:UpdateAccessKey",
+      "iam:GetAccessKeyLastUsed",
+      "iam:ListAccessKeys",
+    ]
+    resources = [
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "AllowManageOwnSSHPublicKeys"
+    effect = "Allow"
+    actions = [
+      "iam:DeleteSSHPublicKey",
+      "iam:GetSSHPublicKey",
+      "iam:ListSSHPublicKeys",
+      "iam:UpdateSSHPublicKey",
+      "iam:UploadSSHPublicKey",
+    ]
+    resources = [
+      "arn:aws:iam::*:user/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "AllowMFADeviceHandling"
+    effect = "Allow"
+    actions = [
+      "iam:DeactivateMFADevice",
+      "iam:DeleteVirtualMFADevice",
+      "iam:EnableMFADevice",
+      "iam:ResyncMFADevice",
+      "iam:CreateVirtualMFADevice",
+      "iam:ListMFADevices",
+    ]
+    resources = [
+      "arn:aws:iam::*:mfa/$${aws:username}",
+    ]
+  }
+  statement {
+    sid    = "AllowMFAListing"
+    effect = "Allow"
+    actions = [
+      "iam:ListVirtualMFADevices",
+    ]
+    resources = [
+      "*",
+    ]
+  }
 }
