7b5d024f5c030652f6cff19a3866d9b092beeaa3
Dirk Piethan <dirk@centrias.de>
2022-07-22 17:38:40 +0200

    new resource handling for awes_s3_bucket_* resources

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -10,24 +10,6 @@ resource "aws_iam_role_policy_attachment" "config" {
 #tfsec:ignore:AWS002
 resource "aws_s3_bucket" "config" {
   bucket_prefix = var.config_bucket_name_prefix
-  acl           = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    enabled = true
-    prefix  = "${var.config_bucket_key_prefix}/"
-
-    expiration {
-      days = 365
-    }
-
-    noncurrent_version_expiration {
-      days = 365
-    }
-  }
 
   tags = merge(
     var.common_tags,
@@ -35,15 +17,54 @@ resource "aws_s3_bucket" "config" {
       "Name" = "Config Bucket"
     },
   )
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_acl" "config_acl" {
+  bucket = aws_s3_bucket.config.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "config_versioning" {
+  bucket = aws_s3_bucket.config.id
+
+  versioning_configuration {
+    status     = "Enabled"
+    mfa_delete = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "config_lc" {
+  bucket = aws_s3_bucket.config.id
+
+  rule {
+    id     = "Config_LifeCycle"
+    status = "Enabled"
+
+    filter {
+      prefix = "${var.config_bucket_key_prefix}/"
+    }
+
+    expiration {
+      days = 365
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 365
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "config_sse" {
+  bucket = aws_s3_bucket.config.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
+
 
 resource "time_sleep" "wait_for_bucket_created" {
   depends_on      = [aws_s3_bucket.config]
